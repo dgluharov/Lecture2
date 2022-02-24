026657c2bb3cfa6d31fde155017d0af576a0615a
Doncho.Gluharov <dgluharov@gmail.com>
2022-02-24 22:07:25 +0200

    Lecture 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,62 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         int randomNumber = ThreadLocalRandom.current().nextInt(-100, 100 + 1);
 
-        System.out.println("The random number is:" + randomNumber);
-
         if (randomNumber < 0) {
-            System.out.println("The number is negative");
+            System.out.println("The number " + randomNumber + " is negative");
         } else {
-            System.out.println("The number is positive");
+            System.out.println("The number " + randomNumber + " is positive");
         }
+
+        String theNumberIs = (randomNumber < 0) ? "The number " + randomNumber + " is negative" : "The number " + randomNumber + " is positive";
+
+        System.out.println(theNumberIs);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = scanner.nextInt();
+
+        if (grade == 6) {
+            System.out.println("Your grade is Excellent");
+        } else if (grade == 5) {
+            System.out.println("Your grade is Very good");
+        } else if (grade == 4) {
+            System.out.println("Your grade is Good");
+        } else if (grade == 3) {
+            System.out.println("Your grade is Average");
+        } else {
+            System.out.println("Your grade is Poor");
+        }
+
+        int grade2 = scanner.nextInt();
+        switch (grade2) {
+            case 2:
+                System.out.println("Your grade is Poor");
+                break;
+            case 3:
+                System.out.println("Your grade is Average");
+                break;
+            case 4:
+                System.out.println("Your grade is Good");
+                break;
+            case 5:
+                System.out.println("Your grade is Very good");
+                break;
+            case 6:
+                System.out.println("Your grade is Excellent");
+                break;
+            default:
+                System.out.println("Invalid number");
+                break;
+        }
+
+        scanner.close();
     }
 }
